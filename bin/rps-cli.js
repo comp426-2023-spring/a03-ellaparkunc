9f8d241ae6c8d13f5b834b7f9e0d835dbddff7ac
Ella Parker <ellapark@live.unc.edu>
2023-04-01 15:35:50 -0400

    both cli's work and i can interact with my other file

---
+++ bin/rps-cli.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node-rps
+
+//file that runs when we link/install package & run node-rps
+
+//import fetch from 'node-fetch';
+import minimist from 'minimist';
+import {rps} from '../lib/rpsls.js';
+
+console.log(rps());
+
+//Grab provided args.
+const args = minimist(process.argv.slice(2));
+
+let helpText = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`
+
+if (args.h || args.help){
+    console.log(helpText);
+    process.exit(0);
+}
+
+let rulesText = `Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`
+
+if (args.r || args.rules){
+    console.log(rulesText);
+    process.exit(0);
+}
+
+//const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&timezone=' + timezone);
+
+// Get the data from the request
+
+//const data = await response.json();
+
+
+console.log(`Hello World`);
+process.exit(0);
+
+//console.log(data);
---
+++ bin/rpsls-cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node-rpsls
+
+
+import minimist from 'minimist';
+
+
+//Grab provided args.
+const args = minimist(process.argv.slice(2));
+
+let helpText = `Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`
+
+if (args.h || args.help){
+    console.log(helpText);
+    process.exit(0);
+}
+
+let rulesText = `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`
+
+if (args.r || args.rules){
+    console.log(rulesText);
+    process.exit(0);
+}
+
+console.log(`Hello World`);
+process.exit(0);
