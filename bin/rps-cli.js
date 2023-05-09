cd3a44b65b21940cabe7b56204f8008bdd22f5ac
ellaparker1 <89318755+ellaparkunc@users.noreply.github.com>
2023-05-09 10:50:21 -0400

    work from yesterday

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -11,11 +11,10 @@ import {rps} from '../lib/rpsls.js';
 
 console.log(rps());
 
-//hello
-
 //Grab provided args.
 const args = minimist(process.argv.slice(2));
 
+console.log(args)
 let helpText = `Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
 
@@ -51,6 +50,9 @@ if (args.r || args.rules){
 //const data = await response.json();
 
 //if no arguments,
+
+let shot = args._[0]
+
 if (!args.h || !args.help || !args.r || !args.rules ){
   console.log(`please provide an argument`);
   console.log(`{"player":"rock"}`)
